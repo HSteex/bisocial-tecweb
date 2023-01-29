2301af73a9a0402c339ee8bae9e4324ae6a77ec0
lrubboli <lrubboli@imolainformatica.it>
2023-01-30 00:18:11 +0100

    Mockup upload, database comments, css retouch, last code cleaning

--- src/database.php
+++ src/database.php
@@ -9,12 +9,14 @@ class DatabaseHelper{
         }*/
     }
 
+    // Add user to db
     public function addUser($username, $email, $password, $salt) {
         $insert_stmt = $this->db->prepare("INSERT INTO user (username, email, password, salt) VALUES (?, ?, ?, ?)");
         $insert_stmt->bind_param('ssss', $username, $email, $password, $salt);
         $insert_stmt->execute();
     }
 
+    //Get user
     public function getUser($username) {
         $stmt = $this->db->prepare("SELECT user_id, username, password, salt FROM user WHERE username = ? LIMIT 1");
         $stmt->bind_param('s', $username);
@@ -23,6 +25,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get user info
     public function getUserInfo($user_id) {
         $stmt = $this->db->prepare("SELECT email, password, salt, nome, cognome, bio, user_image  FROM user WHERE user_id = ? LIMIT 1");
         $stmt->bind_param('i', $user_id);
@@ -31,6 +34,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get user detail
     public function getUserDetail($username){
         $stmt = $this->db->prepare("SELECT user_id, username, nome,cognome ,bio,user_image FROM user WHERE username = ? LIMIT 1");
         $stmt->bind_param('s', $username);
@@ -39,6 +43,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get user followers
     public function getFollowers($user_id) {
         $stmt = $this->db->prepare("SELECT u.username, u.user_image, u.nome, u.cognome
         FROM user u
@@ -51,6 +56,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get people that user follow
     public function getFollowing($user_id){
         $stmt=$this->db->prepare("SELECT u.username, u.user_image, u.nome, u.cognome
         FROM user u
@@ -63,6 +69,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get user post
     public function getPostsOfPersonal($user_id){
         $stmt = $this->db->prepare("SELECT u.username, u.user_image, u.nome, u.cognome ,p.* 
         from user u 
@@ -75,6 +82,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get single post
     public function getSinglePost($post_id){
         $stmt = $this->db->prepare("SELECT u.username, u.user_image, u.nome, u.cognome ,p.* 
         from user u 
@@ -86,6 +94,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get post of people that user follow
     public function getPostsOfFollowing($user_id){
         $stmt = $this->db->prepare("SELECT u.username, u.user_image, u.nome, u.cognome, p.*
         from follower f 
@@ -114,7 +123,7 @@ class DatabaseHelper{
         }
     }
 
-    //Save notification details after user trigger
+    //Save notification details after user trigger (comment)
     public function saveNotify($user_id, $target_id, $content, $href){
         $stmt = $this->db->prepare("INSERT INTO `notif` (source_id, target_id, content, href) VALUES (?, ?, ?, ?)");
         $stmt->bind_param('iiss', $user_id, $target_id, $content, $href);
@@ -133,6 +142,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);  
     }
 
+    //Count the number of notification
     public function countNotification($user_id){
         $stmt = $this->db->prepare("SELECT COUNT(*) as count FROM notif WHERE target_id = ?");
         $stmt->bind_param('i', $user_id);
@@ -142,20 +152,21 @@ class DatabaseHelper{
 
     }
 
+    //Delete signle notification
     public function deleteNotification($notif_id){
         $stmt = $this->db->prepare("DELETE FROM notif WHERE notif_id = ?");
         $stmt->bind_param('i', $notif_id);
         $stmt->execute();
     }
 
+    //Delete all notification
     public function deleteAllNotifications($user_id){
         $stmt = $this->db->prepare("DELETE FROM notif WHERE target_id = ?");
         $stmt->bind_param('i', $user_id);
         $stmt->execute();
     }
 
-
-
+    //Action to add or remove like
     public function toggleLike($user_id, $post_id, $like_bool){
         if(!$like_bool){
             $stmt = $this->db->prepare("INSERT INTO `like` (user_id, post_id) VALUES (?, ?)");
@@ -193,6 +204,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC)[0]["COUNT(*)"];
     }
 
+    //Get number of followers
     public function getFollowersCount($user_id){
         $follow=array();
         $stmt = $this->db->prepare("SELECT COUNT(*) FROM follower WHERE target_id = ?");
@@ -208,10 +220,12 @@ class DatabaseHelper{
         return $follow;
     }
 
+    //Action to add or remove follow
     public function toggleFollow($user_id, $target_id, $flag) {
         $stmt = $this->db->prepare("SELECT  follower (source_id, target_id) VALUES (?, ?)");
     }
 
+    //Action to follow
     public function follow($user_id, $target_id) {
         try {
             $stmt = $this->db->prepare("INSERT INTO follower (source_id, target_id) VALUES (?, ?)");
@@ -226,12 +240,14 @@ class DatabaseHelper{
         }
     }
 
+    //Action to unfollow
     public function unfollow($user_id, $target_id) {
         $stmt = $this->db->prepare("DELETE FROM follower WHERE source_id = ? AND target_id = ?");
         $stmt->bind_param('ii', $user_id, $target_id);
         $stmt->execute();
     }
 
+    //Get user by email
     public function getUserByEmail($email) {
         $stmt = $this->db->prepare("SELECT user_id, username, email, password, salt FROM user WHERE email = ? LIMIT 1");
         $stmt->bind_param('s', $username);
@@ -240,6 +256,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get user image
     public function getImage($user_id) {
         $stmt = $this->db->prepare("SELECT user_image FROM user WHERE user_id = ? LIMIT 1");
         $stmt->bind_param('i', $user_id);
@@ -248,6 +265,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Get last post by ID
     public function getLastPostID() {
         $stmt = $this->db->prepare("SELECT post_id FROM post ORDER BY post_id DESC LIMIT 1");
         $stmt->execute();
@@ -255,12 +273,14 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Add post
     public function addPost($user_id, $description, $created_at, $post_image) {
         $insert_stmt = $this->db->prepare("INSERT INTO post (creator_id, description, created_at, post_image) VALUES (?, ?, ?, ?)");
         $insert_stmt->bind_param('isss', $user_id, $description, $created_at, $post_image);
         $insert_stmt->execute();
     }
 
+    //Get creator ID by post
     public function getPostOwner($post_id){
         $stmt = $this->db->prepare("SELECT creator_id from post where post_id =?");
         $stmt->bind_param('i', $post_id);
@@ -269,6 +289,7 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC)[0]["creator_id"];
     }
 
+    //Get comments
     public function getComments($post_id) {
         $stmt = $this->db->prepare("SELECT c.user_id, u.username, u.nome, u.cognome, u.user_image, content FROM comment c
                                           JOIN user u ON c.user_id = u.user_id
@@ -280,17 +301,19 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //Add comment
     public function addComment($post_id, $user_id, $content, $created_at) {
         $insert_stmt = $this->db->prepare("INSERT INTO comment (post_id, user_id, content, created_at) VALUES (?, ?, ?, ?)");
         $insert_stmt->bind_param('iiss', $post_id, $user_id, $content, $created_at);
         $insert_stmt->execute();
     }
 
-
+    //Add login attempt to db
     public function addAttempt($user_id, $now) {
         $this->db->query("INSERT INTO login_attempts (user_id, time) VALUES ('$user_id', '$now')");
     }
 
+    //Update profile info
     public function updateProfile($user_id, $email, $password, $salt, $nome, $cognome, $bio, $image) {
         $insert_stmt = $this->db->prepare("UPDATE user SET email=?,password=?,salt=?,nome=?,cognome=?,bio=?,user_image=?
                                                  WHERE user_id=?");
@@ -298,6 +321,7 @@ class DatabaseHelper{
         $insert_stmt->execute();
     }
 
+    //Check bruteforcing attempts
     public function checkBrute($user_id) {
         $now = time();
         // Vengono analizzati tutti i tentativi di login a partire dalle ultime due ore.
